7b1e01751c4aa6caa761a1974afe4e502a442aa3
Akinwale Samuel <wale0331@gmail.com>
2019-04-29 15:12:29 +0100

    164797972-chore(sass): Setup sass
    - add sass loader
    - css loader
    
    [Finishes #164797972]

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   resolve: {
