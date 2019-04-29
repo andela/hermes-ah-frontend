694c1aeceb747cfd9755c108df4c013d5eaaffe1
Adesoji Daniel <sojidaniels@gmail.com>
2019-04-29 13:52:45 +0100

    164797967-chore(webpack): setup webpack - setup eslint - setup prettier

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './src/index.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/public`,
+    publicPath: '/',
+    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: './public',
+    hot: true,
+    historyApiFallback: true,
+  },
+};
