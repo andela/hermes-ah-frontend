fc353e47b0b22f994313580861db3cd6a9a0254a
Adesoji Daniel <sojidaniels@gmail.com>
2019-04-30 12:01:14 +0100

    164797967-chore(webpack): update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,7 @@
-const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.common');
 
-module.exports = {
-  entry: {
-    index: './src/index.jsx',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[hash]-[name].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    path: `${__dirname}/public`,
-    publicPath: '/',
-    filename: 'bundle.js',
-    chunkFilename: '[name].bundle.js',
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  devServer: {
-    contentBase: './public',
-    hot: true,
-    historyApiFallback: true,
-  },
+module.exports = env => {
+  console.log(env);
+  return webpackMerge(commonConfig);
 };
