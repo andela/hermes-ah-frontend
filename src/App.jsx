f3f8229df0c030eed32ef9fbb65a25b3bcb4fdd6
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-04-30 12:33:00 +0100

    164797971-chore(fix): fix failing test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import store from './utils/store';
 import Homepage from './components/containers/Homepage/Homepage';
 import Login from './components/containers/Login/Login';
@@ -15,12 +20,14 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/not-found" component={Notfound} />
-          <Route path="/" exact component={Homepage} />
-          <Redirect to="/not-found" />
-        </Switch>
+        <Router>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/not-found" component={Notfound} />
+            <Route path="/" exact component={Homepage} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </Router>
       </Provider>
     );
   }
