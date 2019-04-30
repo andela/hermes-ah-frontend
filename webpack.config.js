6728a601cac1e440a05bd7acf395c0b71128e2ce
Adesoji Daniel <sojidaniels@gmail.com>
2019-04-30 13:33:26 +0100

    164797967-chore(webpack): update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
-module.exports = env => {
-  console.log(env);
-  return webpackMerge(commonConfig);
+module.exports = () => {
+  const env = process.env.NODE_ENV;
+  let envConfig;
+  if (!env) {
+    envConfig = require('./webpack/webpack.development');
+  } else {
+    envConfig = require(`./webpack/webpack.${env}`);
+  }
+
+  return webpackMerge({ mode: env }, commonConfig, envConfig);
 };
