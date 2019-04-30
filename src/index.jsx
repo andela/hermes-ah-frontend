28c4a4e7a251e73ef6577eb0b2a7962acce78d4d
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-04-30 14:13:24 +0100

    164797971-chore(fix): fix eol

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
-  // <Router>
-  <App />,
-  // </Router>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
 
 module.hot.accept();
