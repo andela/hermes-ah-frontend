bb8e1ee734e1b589d539153092d7bc3a6e160a73
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-04-30 11:39:17 +0100

    164797971-chore(route): setup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './utils/store';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +16,14 @@ class App extends Component {
           <h1>Welcome to Ah</h1>
         </div>
       </Provider>
+      <div>
+        <Switch>
+          <Route path="/login" component="" />
+          <Route path="/not-found" component="" />
+          <Route path="/" exact component="" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
     );
   }
 }
