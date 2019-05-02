566b2a91c3d3871bdfdbfada69496a080c9e858c
mchardex <mchardex1995@gmail.com>
2019-05-02 10:26:43 +0100

    165727294-feature(feature): create reusable navbar

---
+++ src/components/shared/NavBar/NavBar.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import propTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/logo_transparent.png';
+// import profileImage from '../../../assets/images/Screenshot 2019-04-08 at 10.57.29 AM.png';
+import './navbar.scss';
+
+class NavBar extends Component {
+  state = {};
+
+  render() {
+    const { children } = this.props;
+    return (
+      <Menu>
+        <Menu.Menu>
+          <Menu.Item>
+            <img className="logo" src={logo} alt="logo" size="small" />
+          </Menu.Item>
+        </Menu.Menu>
+        <Menu.Menu position="right">{children}</Menu.Menu>
+      </Menu>
+    );
+  }
+}
+
+NavBar.defaultProps = {
+  children: (propTypes.defaultProps = ''),
+};
+
+NavBar.propTypes = {
+  children: propTypes.oneOfType([propTypes.object, propTypes.string]),
+};
+
+export default NavBar;
+
+/* <Header>
+    <Image circular src={logo} />
+    <List link>
+      <Link to="/">
+        <List.Item>ABOUT</List.Item>
+      </Link>
+      <Link to="/">
+        <List.Item>CATEGORIES</List.Item>
+      </Link>
+      <Link to="/">
+        <List.Item>LOGIN</List.Item>
+      </Link>
+      <Link to="/">
+        <List.Item active>SIGNUP</List.Item>
+      </Link>
+    </List>
+  </Header> */
