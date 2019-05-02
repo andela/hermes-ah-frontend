3c75247a8d2e150fa7fd6c5f55c5f226389ffca6
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-02 16:54:22 +0100

    165770236-feature(about-page):about page - create file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
-import store from './utils/store';
-import Homepage from './components/containers/Homepage/Homepage';
-import Login from './components/containers/Login/Login';
-import Notfound from './components/containers/Notfound/Notfound';
+// import { Provider } from 'react-redux';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+//   Redirect,
+// } from 'react-router-dom';
+// import store from './utils/store';
+// import Homepage from './components/containers/Homepage/Homepage';
+// import Login from './components/containers/Login/Login';
+// import Notfound from './components/containers/Notfound/Notfound';
+import AboutPage from './components/presentations/AboutPage/AboutPage';
 
 class App extends Component {
   constructor(props) {
@@ -19,16 +20,17 @@ class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/not-found" component={Notfound} />
-            <Route path="/" exact component={Homepage} />
-            <Redirect to="/not-found" />
-          </Switch>
-        </Router>
-      </Provider>
+      <AboutPage />
+      // <Provider store={store}>
+      //   <Router>
+      //     <Switch>
+      //       <Route path="/login" component={Login} />
+      //       <Route path="/not-found" component={Notfound} />
+      //       <Route path="/" exact component={Homepage} />
+      //       <Redirect to="/not-found" />
+      //     </Switch>
+      //   </Router>
+      // </Provider>
     );
   }
 }
---
+++ src/components/presentations/AboutPage/AboutPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './about-page.scss';
+
+const AboutPage = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <section />
+        <section className="bottom-section">
+          <div className="img" />
+          <div className="text-container">
+            <p className="text">
+              A CUSTOMIZABLE READING EXPERIENCE MADE JUST FOR YOU
+            </p>
+          </div>
+        </section>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AboutPage;
