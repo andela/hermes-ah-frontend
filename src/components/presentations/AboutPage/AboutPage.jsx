0d2b3ca76e890ffbaa8f4162ae369be9a78c699c
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-03 16:35:06 +0100

    165770236-feature(about-page):about page - add description - add cards

--- src/components/presentations/AboutPage/AboutPage.jsx
+++ src/components/presentations/AboutPage/AboutPage.jsx
@@ -5,12 +5,57 @@ const AboutPage = () => {
   return (
     <React.Fragment>
       <div>
-        <section>
-          <div>
-            <div className="typewriter" />
-            <h1>HELLO</h1>
-          </div>
-        </section>
+        {/* <div>
+          <h1>HELLO</h1>
+          <h1>HELLO</h1>
+        </div> */}
+        <span className="circle" />
+        <div className="title">
+          <h1>Welcome to Authors Haven</h1>
+          <div className="image" />
+          <hr />
+          <p className="desc">
+            A home for bright minds to make their contributions
+            <br />
+            to the world profer solutions
+          </p>
+          <p className="desc">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+            voluptatum dolorem et odio
+            <br />
+            ut rerum eaque rem, temporibus neque neque corporis omnis iste saepe
+            ab, reiciendis nostrum
+            <br />
+            eveniet harum doloribus sapiente.
+          </p>
+        </div>
+        <br />
+        <div>
+          <div className="typewriter" />
+        </div>
+        <div>
+          <p className="textHeader" id="first-category">
+            Biology
+          </p>
+          <p className="textHeader" id="first-article">
+            Genetic Mutation fills the space
+          </p>
+          <div className="cards" id="left" />
+          <p className="textHeader" id="second-category">
+            Chemistry
+          </p>
+          <p className="textHeader" id="second-article">
+            Why Kinetic motivates the nitro
+          </p>
+          <div className="cards" id="middle" />
+          <p className="textHeader" id="third-category">
+            Neurology
+          </p>
+          <p className="textHeader" id="third-article">
+            The power of the brain
+          </p>
+          <div className="cards" id="right" />
+        </div>
         <section className="bottom-section">
           <div className="img" />
           <div className="text-container">
