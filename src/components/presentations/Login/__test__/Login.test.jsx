01af47b9d1e867c2a6f8352b52a5c90c0c9b80c0
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-05-08 18:36:07 +0100

    164797975-feature(verification): fix failing test

--- src/components/presentations/Login/__test__/Login.test.jsx
+++ src/components/presentations/Login/__test__/Login.test.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Login from '../Login';
 
 describe('Login component', () => {
   it('should match snapshot', () => {
     const wrap = <Login />;
     expect(wrap).toMatchSnapshot();
+  });
+  it('should change state', () => {
+    const wrapper = shallow(<Login isLoadingReducer={{ loader: true }} />);
+    expect(wrapper.find('div'));
+    expect(wrapper.state('data')).toEqual({
+      email: '',
+      password: '',
+    });
   });
 });
