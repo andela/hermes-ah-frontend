ccac862c1e33077392289f506b1dddfea8d897e6
Adesoji Daniel <sojidaniels@gmail.com>
2019-05-08 20:54:14 +0100

    164797979-feature(profile): navbar dropdown

--- src/components/shared/NavBar/__test__/NavBar.test.jsx
+++ src/components/shared/NavBar/__test__/NavBar.test.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { create } from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from '../NavBar';
 
 const user = { name: null };
 
+const userProfile = {
+  userProfile: {
+    profile: {
+      first_name: 'jest',
+      last_name: 'enzyme',
+    },
+  },
+};
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore({ userProfile });
+
 describe('NavBar component', () => {
   it('should match snapshot', () => {
     const wrapper = create(
       <Router>
-        <NavBar user={user} />
+        <Provider store={store}>
+          <NavBar user={user} />
+        </Provider>
       </Router>
     );
     expect(wrapper.toJSON).toMatchSnapshot();
