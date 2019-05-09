23c1b1ec06277f26043e105288d3e0fbcb1c7118
Adesoji Daniel <sojidaniels@gmail.com>
2019-05-09 12:48:24 +0100

    164797979-feature(profile): prile image update

--- src/components/presentations/Profilepage/ProfilePage.jsx
+++ src/components/presentations/Profilepage/ProfilePage.jsx
@@ -17,7 +17,7 @@ class Profilepage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentTab: 'following-section',
+      currentTab: 'profile-section',
       firstname: '',
       lastname: '',
       profilePic:
