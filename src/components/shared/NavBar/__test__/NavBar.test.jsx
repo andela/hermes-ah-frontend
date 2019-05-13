e36a48f6a3ffaa444f84af7c089993aafd5255ec
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-05-13 18:42:29 +0100

    165904619-feature(admin-page): implement admin profile page menu - update falling test

--- src/components/shared/NavBar/__test__/NavBar.test.jsx
+++ src/components/shared/NavBar/__test__/NavBar.test.jsx
@@ -58,6 +58,11 @@ describe('NavBar component', () => {
   });
 
   it('should change state', () => {
+    localStorage.setItem(
+      'token',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyT2JqIjp7ImlkIjoiNTdjNTE1YTEtODkwZC00MTJmLThjYTEtMGE1Mzk1MTIzZGNhIiwiZW1haWwiOiJhbWVhY2hpY2h1a3NAZ21haWwuY29tIiwiaXNBZG1pbiI6dHJ1ZSwiaXNSZXZpZXdlciI6dHJ1ZSwiaXNBY3RpdmF0ZWQiOnRydWV9LCJpYXQiOjE1NTc3Njg3NjcsImV4cCI6MTU1ODAyNzk2N30.msqGVSZbPrBu8ky7Pa-KauhDnMLGRHG4eas9wlIEEA0'
+    );
+
     const wrapper = shallow(
       <NavContainer
         userProfile={initUserProfile}
