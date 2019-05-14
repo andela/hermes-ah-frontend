8bc9db471b8e13b46bd294a4ea6c1cfecb451feb
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-14 09:39:35 +0100

    164797980-(creat-article):create article - updqte config

--- jest.config.js
+++ jest.config.js
@@ -49,6 +49,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/fileMock.js',
+    '.+\\.(css)$': '<rootDir>/mocks/fileMock.js',
   },
 
   collectCoverage: true,
