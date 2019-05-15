61fb701e5d2ab142ce17c54c57b5376061127541
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-15 20:13:04 +0100

    164797980-feature(create-article):user create article - fix css

--- src/components/presentations/NewArticle/NewArticleForm/NewArticleForm.jsx
+++ src/components/presentations/NewArticle/NewArticleForm/NewArticleForm.jsx
@@ -24,7 +24,7 @@ const NewArticleForm = ({
           <div className="row">
             <div className="col-12 col-lg-12">
               <form className="p-30 bg-gray rounded" id="whole-form">
-                <div className="row">
+                <div className="input-row">
                   <div>
                     <h3 className="input-heading">Title</h3>
                     <Input
@@ -44,7 +44,7 @@ const NewArticleForm = ({
                   </div>
                 </div>
                 <br />
-                <div className="form-group">
+                <div className="editor-group">
                   <h3 className="input-heading">Body</h3>
                   <Editor
                     onEditorStateChange={onEditorStateChange}
@@ -52,50 +52,54 @@ const NewArticleForm = ({
                   />
                 </div>
                 <br />
-                <div>
-                  <h3 className="input-heading">Keywords</h3>
-                  <Dropdown
-                    placeholder="keywords"
-                    name="category"
-                    fluid
-                    multiple
-                    search
-                    selection
-                    options={keywordsOptions}
-                    onChange={saveKeywords}
-                  />
-                  <p className="keyword-instruction">
-                    Enter a comma-separated list. For example: Anatomy,
-                    Bioinformatics
-                  </p>
-                </div>
-                <br />
-                <div>
-                  <h3 className="input-heading">Category</h3>
-                  <Dropdown
-                    placeholder="Select category"
-                    fluid
-                    search
-                    selection
-                    options={categoryOptions}
-                    onChange={saveCategory}
-                    className="input-holders"
-                  />
+                <div className="dropdown-group">
+                  <div>
+                    <h3 className="input-heading">Keywords</h3>
+                    <Dropdown
+                      placeholder="keywords"
+                      name="category"
+                      fluid
+                      multiple
+                      search
+                      selection
+                      options={keywordsOptions}
+                      onChange={saveKeywords}
+                    />
+                    <p className="keyword-instruction">
+                      Enter a comma-separated list. For example: Anatomy,
+                      Bioinformatics
+                    </p>
+                  </div>
+                  <br />
+                  <div>
+                    <h3 className="input-heading">Category</h3>
+                    <Dropdown
+                      placeholder="Select category"
+                      fluid
+                      search
+                      selection
+                      options={categoryOptions}
+                      onChange={saveCategory}
+                      className="input-holders"
+                    />
+                  </div>
                 </div>
                 <br />
                 <h3 className="input-heading">Article Header Image</h3>
-                <div className="image-upload">
-                  <div className="image-upload-box">
-                    <input
-                      type="file"
-                      name="image"
-                      className="form-control"
-                      onChange={saveToCloudinary}
-                    />
+                <div className="image-group">
+                  <div className="image-upload">
+                    <div className="image-upload-box">
+                      <input
+                        type="file"
+                        name="image"
+                        className="form-control"
+                        onChange={saveToCloudinary}
+                      />
+                    </div>
+                    <p>One file only</p>
+                    <p>2 MB limit</p>
+                    <p>Allowed types: png, gif, jpg, jpeg</p>
                   </div>
-                  <p>One file only</p>
-                  <p>2 MB limit</p>
-                  <p>Allowed types: png, gif, jpg, jpeg</p>
                 </div>
                 <br />
                 <br />
