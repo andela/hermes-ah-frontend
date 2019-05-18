c94e059045fbd384d77652d7c271b792bddf11d4
Adesoji Daniel <sojidaniels@gmail.com>
2019-05-18 14:47:30 +0100

    166103435-feature(article): grid article page

---
+++ src/components/presentations/Article/ArticlePage.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import NavBar from '../../shared/NavBar/NavBar';
+import './Article.scss';
+
+class ArticlePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="custom-center article-page">
+          <Grid>
+            <Grid.Row columns={3}>
+              <Grid.Column computer={2} mobile={16}>
+                <div className="section">Social Section</div>
+              </Grid.Column>
+              <Grid.Column computer={10} mobile={16}>
+                <Grid.Row>
+                  <Grid.Column>
+                    <div className="section">Reading Section</div>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <div className="section">Comment Section</div>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid.Column>
+              <Grid.Column computer={4} mobile={16}>
+                <div className="section">Related article section</div>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ArticlePage;
