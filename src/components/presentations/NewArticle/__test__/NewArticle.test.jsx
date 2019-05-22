7e9c14c5525e207b59bcabb6ec0d2a55f6976258
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-22 21:05:28 +0100

    chore(implement-feedback):implement feedback - increase test coverage

---
+++ src/components/presentations/NewArticle/__test__/NewArticle.test.jsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+// eslint-disable-next-line no-unused-vars
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import NewArticleForm from '../NewArticleForm/NewArticleForm';
+import NewArticle from '../NewArticle';
+import mock from '../../../../utils/testMocks';
+
+// eslint-disable-next-line no-unused-vars
+const { newArticle } = mock;
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+// eslint-disable-next-line no-unused-vars
+const store = mockStore({});
+
+describe('newArticle component', () => {
+  it('should call onChange prop', () => {
+    const props = {
+      postArticle: jest.fn(),
+      success: true,
+      history: {
+        push: '/',
+      },
+    };
+    const event = {
+      preventDefault: jest.fn(),
+      target: { name: 'title', value: 'The-value' },
+    };
+    const component = shallow(<NewArticle {...props} />);
+    expect(component.instance().state.title).toEqual('');
+    component.instance().onChange(event);
+    expect(component.instance().state.title).toEqual('the-value');
+  });
+
+  it('should call handleAddition prop', () => {
+    const props = {
+      postArticle: jest.fn(),
+      success: true,
+      history: {
+        push: '/',
+      },
+    };
+    const event = {
+      preventDefault: jest.fn(),
+      target: { options: [{ text: 'title', value: 'title' }] },
+    };
+    const component = shallow(<NewArticle {...props} />);
+    // expect(component.instance().state.text).toEqual('');
+    expect(component.state().keywords).toEqual([]);
+    component.instance().handleAddition(event, { value: 'title' });
+    // console.log(component.instance().state.options[0].value);
+    // expect(component.instance().state.text).toEqual('title');
+    expect(component.state().options[0].value).toBe('title');
+  });
+
+  it('should call handleChange prop', () => {
+    const props = {
+      postArticle: jest.fn(),
+      success: true,
+      history: {
+        push: '/',
+      },
+    };
+    const event = {
+      preventDefault: jest.fn(),
+      target: {
+        keywords: [{ value: 'title' }],
+      },
+    };
+    const component = shallow(<NewArticle {...props} />);
+    expect(component.state().keywords).toEqual([]);
+    component.instance().handleChange(event, { value: ['title'] });
+    expect(component.instance().state.keywords[0]).toEqual('title');
+    // expect(component.instance().state.keywords.value).toEqual('title');
+  });
+
+  //   it.only('should call saveCategory prop', () => {
+  //     const props = {
+  //       postArticle: jest.fn(),
+  //       success: true,
+  //       history: {
+  //         push: '/',
+  //       },
+  //     };
+  //     // eslint-disable-next-line no-console
+  //     // eslint-disable-next-line no-undef
+  //     const event = {
+  //       preventDefault: jest.fn(),
+  //       target: {
+  //         attributes: {
+  //           getNamedItem(name) {
+  //             return {
+  //               value: name,
+  //             };
+  //           },
+  //         },
+  //       },
+  //     };
+
+  //     const component = shallow(<NewArticle {...props} />);
+  //     // expect(component.instance().state.text).toEqual('');
+  //     expect(component.state().category).toEqual('');
+  //     component.instance().saveCategory(event, { value: ['title'] });
+  //     // console.log(
+  //   component.instance().state.category);
+  //     // expect(component.instance().state.text).toEqual('title');
+  //     expect(component.state().category.value).toBe(['title']);
+  //   });
+
+  it('should call saveOrPublish prop', () => {
+    const article = {
+      title: 'Sciencetist dont die',
+      abstract: 'Science is always alive',
+      imageUrl:
+        'https://res.cloudinary.com/dnch08bzc/image/upload/v1556821917/3d-rendering-human-brain-technology-background_34663-72.jpg',
+      body: 'Lorem ipsum lorem ipsum lorem ipsum',
+      keywords: ['science', 'fiction', 'vision'],
+      category: 'Science',
+    };
+
+    const props = {
+      postArticle: jest
+        .fn()
+        .mockReturnValue(Promise.resolve({ data: article })),
+      success: true,
+      history: {
+        push: '/',
+      },
+    };
+
+    const event = {
+      preventDefault: jest.fn(),
+    };
+
+    const component = shallow(<NewArticle {...props} />);
+    component.instance().saveOrPublish(event, { isDraft: false });
+    component.instance().setState({
+      ...article,
+    });
+    expect(component.state('title')).toEqual('Sciencetist dont die');
+  });
+});
+
+describe('NewArticlePage component', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow(<NewArticleForm />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<NewArticleForm />);
+    expect(wrapper.find('div'));
+    expect(wrapper.find('main'));
+    expect(wrapper.find('form'));
+    expect(wrapper.hasClass('title'));
+    expect(wrapper.find('onClick'));
+  });
+});
