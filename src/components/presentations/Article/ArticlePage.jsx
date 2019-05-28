97f97dc31e385cb2bbcf5136e2727c1254404808
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-05-28 13:43:22 +0100

    164797984-feature(like): update test

--- src/components/presentations/Article/ArticlePage.jsx
+++ src/components/presentations/Article/ArticlePage.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import NavBar from '../../shared/NavBar/NavBar';
 import ViewComment from './ViewComment';
@@ -17,6 +19,19 @@ class ArticlePage extends Component {
     await getSingleArticle(articleId);
   }
 
+  componentWillUnmount() {
+    const { reset } = this.props;
+    reset();
+  }
+
+  sortComment = comment => {
+    const sortedComment = comment.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+    return sortedComment;
+  };
+
   render() {
     const {
       singleArticle,
@@ -29,17 +44,17 @@ class ArticlePage extends Component {
       likeArticle,
     } = this.props;
     const { articleId } = match.params;
-    const { article, comments } = singleArticle;
+    const { article, comments, error } = singleArticle;
     const { userProfile } = user;
     const { profile } = userProfile;
 
+    if (error) {
+      return <Redirect to="/notfound" />;
+    }
+
     const { loader: isLoading } = isLoadingReducer;
 
-    // sort comment based on the greater time in descending order
-    const sortComment = comments.sort(
-      (a, b) =>
-        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-    );
+    const url = window.location.href;
     return (
       <React.Fragment>
         <NavBar />
@@ -49,28 +64,49 @@ class ArticlePage extends Component {
             <Grid.Row columns={3}>
               <Grid.Column computer={2} mobile={16}>
                 <div className="section">
-                  <div className="social-sharing">
-                    <div className="facebook-share">
-                      <i className="fab fa-facebook-f" />
+                  {article.author && (
+                    <div className="social-sharing">
+                      <div className="facebook-share">
+                        <a
+                          href={`https://www.facebook.com/dialog/feed?app_id=370972486842144
+                          &redirect_uri=https://develop--hermes-ah.netlify.com
+                          &link=${url}
+                          &picture=${article.image_url}
+                          &caption=${article.abstract}`}
+                          target="_blank"
+                        >
+                          <i className="fab fa-facebook-f" />
+                        </a>
+                      </div>
+                      <div className="twitter-share">
+                        <a
+                          href={`https://twitter.com/intent/tweet?text=${
+                            article.title
+                          } by ${article.author.first_name}${
+                            article.author.last_name
+                          } ${url}`}
+                          target="_blank"
+                          data-size="large"
+                        >
+                          <i className="fab fa-twitter" />
+                        </a>
+                      </div>
+                      <div className="bookmark">
+                        <i className="far fa-bookmark" />
+                      </div>
+                      <div className="like">
+                        <a
+                          href="#"
+                          onClick={() => {
+                            likeArticle(article.id);
+                          }}
+                        >
+                          <i className="far fa-thumbs-up" />
+                        </a>
+                        <p>{article.likes_count}</p>
+                      </div>
                     </div>
-                    <div className="twitter-share">
-                      <i className="fab fa-twitter" />
-                    </div>
-                    <div className="bookmark">
-                      <i className="far fa-bookmark" />
-                    </div>
-                    <div className="like">
-                      <a
-                        href="#"
-                        onClick={() => {
-                          likeArticle(article.id);
-                        }}
-                      >
-                        <i className="far fa-thumbs-up" />
-                      </a>
-                      <p>{article.likes_count}</p>
-                    </div>
-                  </div>
+                  )}
                 </div>
               </Grid.Column>
               <Grid.Column computer={10} mobile={16}>
@@ -101,7 +137,7 @@ class ArticlePage extends Component {
                     />
                   )}
                   <div>
-                    {sortComment.map(comment => (
+                    {this.sortComment(comments).map(comment => (
                       <ViewComment
                         key={comment.id}
                         comment={comment}
@@ -135,6 +171,7 @@ ArticlePage.propTypes = {
   }).isRequired,
   reportArticle: PropTypes.func.isRequired,
   user: PropTypes.shape().isRequired,
+  reset: PropTypes.func.isRequired,
 };
 
 export default ArticlePage;
