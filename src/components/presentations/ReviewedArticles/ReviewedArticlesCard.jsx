d7ba7d9239aa09c9040241a916f37a636789876b
Adesoji Daniel <sojidaniels@gmail.com>
2019-05-31 10:27:33 +0100

    chore(design): update design

--- src/components/presentations/ReviewedArticles/ReviewedArticlesCard.jsx
+++ src/components/presentations/ReviewedArticles/ReviewedArticlesCard.jsx
@@ -60,7 +60,7 @@ const ReviewedArticleCard = ({
               {AcceptBtnValue}
             </button>
           ) : (
-            <button type="submit" className="accept-rev-art">
+            <button type="submit" className="accept-reviewd-art">
               {AcceptBtnValue}
             </button>
           )}
